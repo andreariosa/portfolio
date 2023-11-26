5c7f8ee3df47e3ee403d0fcde621bc04866ea85f
Weping <andrea.riosa@hotmail.it>
2023-11-26 21:56:43 +0100

    Stop custom theme

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## Usage
 
-Clone the repository, run `npm install` and then run `npm start` which will open up a preview in your default browser, watch for changes to core template files, and live reload the browser when changes are saved.
+Clone the repository, run `npm install` and then run `npm start` which will open
+up a preview in your default browser, watch for changes to core template files,
+and live reload the browser when changes are saved.
 
 You can view the `package.json` file to see which scripts are included.
 
@@ -21,3 +23,25 @@ You can view the `package.json` file to see which scripts are included.
 - `git fetch upstream`
 - `git checkout master`
 - `git merge upstream/master`
+
+#### Diff & Merge
+
+For manual compare this repository with the forked one you must prettify the
+original version.
+
+- `git clone <FORK URL>`
+- `cd startbootstrap-freelancer`
+- `npm install`
+- Copy `.prettierrc` from this repository
+- `npx prettier --write .`
+- Use your prefered tool for the comparison
+
+## Useful conversion tools
+
+- [HTML to PUG](https://html2pug.vercel.app)
+- [CSS to SCSS](https://www.cssportal.com/css-to-scss)
+
+## TODO
+
+- [ ] Add custom contact form backend
+- [ ] Add LaTeX resume and PDF build
---
+++ src/assets/img/portfolio/scraper.py
@@ -0,0 +1,51 @@
+import itertools
+
+from selenium import webdriver
+from time import sleep
+from PIL import Image
+
+driver = webdriver.Firefox()
+driver.maximize_window()
+driver.implicitly_wait(30)
+
+# Configs
+
+drDelay = 14.5
+
+urls = ["http://netrooper.com", "http://hitbox.it", "http://iffclan.com", "http://payload.pw", "http://topicron.com"]
+names = ["netrooper", "hitbox", "iffclan", "payload", "topicron"]
+
+imgExt = ".png"
+imgCropRight = 21
+imgCropBottom = 11
+imgResizeW = 900
+
+# Loop
+
+i = 0
+for (u, n) in zip(urls, names):
+    print("\n[" + str(i+1) + "] Visiting: " + u)
+    driver.get(u)
+
+    print("Delay: " + str(drDelay) + " seconds")
+    sleep(drDelay)
+
+    print("Saving: " + n + imgExt)
+    driver.get_screenshot_as_file(n + imgExt)
+
+    imgOriginal = Image.open(n + imgExt)
+    width, height = imgOriginal.size
+    print(" - saved image width: " + str(width) + " px")
+    print(" - saved image height: " + str(height) + " px")
+    imgResizeH = int((imgResizeW * (height - imgCropBottom)) / (width - imgCropRight))
+    print(" - resized image width: " + str(imgResizeW) + " px")
+    print(" - resized image height: " + str(imgResizeH) + " px")
+
+    imgCropped = imgOriginal.crop((0, 0, width - imgCropRight, height - imgCropBottom))
+    imgResized = imgCropped.resize((imgResizeW, imgResizeH))
+    imgResized = imgResized.save(n + imgExt)
+
+    i += 1
+
+driver.quit()
+print("\nFetched " + str(i) + " elements!")
